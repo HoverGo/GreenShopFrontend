587f62f37833b851bc55f608d03bdc7535adebb2
Khanaccount <khanaccount1@mail.ru>
2024-01-10 19:06:17 +0700

    404

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        // Настройка перенаправления всех необработанных запросов на index.html
-        // Это позволит React Router обрабатывать маршруты на клиентской стороне.
-        fs: {
-            strict: false,
-        },
-        proxy: {
-            "/api": {
-                target: "https://greenshopfrontend-production.up.railway.app/", // замените на ваш адрес API
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
-            },
-        },
-        // Перенаправление всех необработанных запросов на index.html
-        // (если это не файл и не начинается с '/api')
-        hmr: {
-            host: "localhost",
-            port: 3000,
-        },
-    },
 });
