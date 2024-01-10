ac1381f8017ab0f60f2e05325942057cb72a1f87
HoverGo <kotikarbuz@gmail.com>
2024-01-10 19:15:35 +0500

    customer avatar

--- src/components/Account/Details/index.tsx
+++ src/components/Account/Details/index.tsx
@@ -37,7 +37,7 @@ const Details: React.FC = () => {
                 };
 
                 const response = await axios.post(
-                    `${API_URL}/customer/avatar/`,
+                    `https://greenshopbackend.up.railway.app/customer/avatar/`,
                     profileImg,
                     config
                 );
