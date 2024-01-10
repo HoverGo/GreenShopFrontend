9865c87d6cb052da84e7a4f194a6438f0c6a9ea3
Khanaccount <khanaccount1@mail.ru>
2024-01-10 10:10:43 +0700

    Убрал лишний /

--- src/components/Shop/ProductCart/index.tsx
+++ src/components/Shop/ProductCart/index.tsx
@@ -270,7 +270,7 @@ const ProductCart: React.FC = () => {
                 .get(`${API_URL}/product/${id}/`, authHeaders)
                 .then((response) => {
                     setProduct(response.data[0]);
-                    setSelectedImage(`/${response.data[0].mainImg}`);
+                    setSelectedImage(response.data[0].mainImg);
 
                     const sizesFromResponse = response.data[0].size || [];
                     setAvailableSizes(sizesFromResponse);
