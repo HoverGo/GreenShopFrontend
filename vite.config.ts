4cd958027619c28f0cbecc20a345770cd0f672c2
Khanaccount <khanaccount1@mail.ru>
2024-01-10 11:20:39 +0700

    попытка фикса 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import ViteSSR from "vite-plugin-ssr/plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: "dist", // Путь до папки, куда будет собран проект
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"), // Путь до вашего index.html
-            },
-        },
-    },
+    plugins: [ViteSSR()],
 });
