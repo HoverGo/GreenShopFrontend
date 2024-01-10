535e570baee53b7529275590f6f7a8690a7a8748
Khanaccount <khanaccount1@mail.ru>
2024-01-10 11:12:51 +0700

    Исправление 404 react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: "dist", // Путь до папки, куда будет собран проект
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"), // Путь до вашего index.html
+            },
+        },
+    },
 });
