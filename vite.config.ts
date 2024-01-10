614a0ead7fad48d62dfeac27a1c4d86e88df4d02
Khanaccount <khanaccount1@mail.ru>
2024-01-10 11:36:04 +0700

    попытка фикса 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
-import ViteSSR from "vite-plugin-ssr/plugin";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ViteSSR()],
+    plugins: [react()],
+    base: "https://greenshopbackend.up.railway.app/",
 });
